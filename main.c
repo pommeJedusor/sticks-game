04995f0294027ff308eb87c0e37fbbcfa1d26e29
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2023-10-07 12:28:27 +0200

    parsing of the main function in other functions and add the possibility to replay

--- main.c
+++ main.c
@@ -2,50 +2,56 @@
 #include <stdio.h>
 #include <string.h>
 
-char get_sticks_number(int number)
+int get_stick_number()
 {
-	return (number-1)%4 + (number%4==1);
+	char response[10];
+	printf("entrez le nombre de stick\n");
+	scanf("%s",response);
+	return atoi(response);
 }
 
-int computer_turn(int stick_number)
+char get_first_player()
 {
-	int best_move = get_sticks_number(stick_number);
-	printf("je retire %d stick(s) il en reste %d\n",best_move,stick_number-best_move);
-	return stick_number - best_move;
+	char response[10];
+	printf("qui commence 0 pour le bot 1 pour vous\n");
+	scanf("%s",response);
+	return atoi(response);
 }
 
-int get_player_input(int stick_number)
+int get_player_move(int stick_number)
 {
 	char response[10];
 	printf("combien de stick(s) souhaitez vous retirer?\n");
 	scanf("%s",response);
 	while ((atoi(response)<1 || atoi(response)>3) || stick_number - atoi(response) <0)
 	{		
-		memset(response,0,strlen(response));
 		printf("combien de stick(s) souhaitez vous retirer?\n");
 		scanf("%s",response);
 	}
 	return atoi(response);
 }
 
+char get_computer_move(int number)
+{
+	return (number-1)%4 + (number%4==1);
+}
+
+int computer_turn(int stick_number)
+{
+	int best_move = get_computer_move(stick_number);
+	printf("je retire %d stick(s) il en reste %d\n",best_move,stick_number-best_move);
+	return stick_number - best_move;
+}
+
 int player_turn(int stick_number)
 {
-	int player_move = get_player_input(stick_number);
+	int player_move = get_player_move(stick_number);
 	printf("vous avez retiré %d stick(s) il en reste %d\n",player_move, stick_number - player_move);
 	return stick_number - player_move;
 }
 
-void main()
+char game(int stick_number, char first_player)
 {
-	printf("entrez le nombre de stick\n");
-	char response[10];
-	scanf("%s",response);
-	int stick_number = atoi(response);
-	
-	printf("qui commence 0 pour le bot 1 pour vous\n");
-	char response2[10];
-	scanf("%s",response2);
-	int first_player = atoi(response2);
 	while (stick_number>0)
 	{
 		if (first_player)
@@ -58,6 +64,11 @@ void main()
 		}
 		first_player = (first_player+1)%2;
 	}
+	return first_player;
+}
+
+void show_winner(int first_player)
+{
 	if (first_player==0)
 	{
 		printf("j'ai gagné\n");
@@ -66,4 +77,27 @@ void main()
 	{
 		printf("vous avez gagné\n");
         }
+}
+
+char want_to_replay()
+{
+	char response[10];
+	printf("voulez vous rejouer? 1 pour oui 0 pour non\n");
+	scanf("%s",response);
+	return atoi(response);
+}
+
+void main()
+{
+	char want_to_play = 1;
+	while (want_to_play)
+	{
+		int stick_number = get_stick_number();	
+		char first_player = get_first_player();
+		
+		first_player = game(stick_number, first_player);
+
+		show_winner(first_player);
+		want_to_play = want_to_replay();
+	}	
 }
